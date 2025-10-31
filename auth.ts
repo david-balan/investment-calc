20ac58185171a3e4b4a9021c393c1b7c5c6e3487
david-balan <davidcbv2004@gmail.com>
2025-10-31 18:03:05 +0200

    a

---
+++ auth.ts
@@ -0,0 +1,29 @@
+import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
+import { authConfig } from './auth.config';
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
+});
+
+export const { GET, POST } = handlers;
